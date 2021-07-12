fea4fb23a4f9cb7298fd42441c702eeeda69219f
xiewenhui777 <2389686970@qq.com>
2021-07-12 21:20:10 +0800

    Initial commit

---
+++ WelcomeDialog.h
@@ -0,0 +1,29 @@
+#ifndef WELCOMEDIALOG_H
+#define WELCOMEDIALOG_H
+
+#include <QDialog>
+#include "mainWidget.h"
+//#include "landwidget.h"
+
+namespace Ui {
+class WelcomeDialog;
+}
+
+class WelcomeDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit WelcomeDialog(QWidget *parent = nullptr);
+    ~WelcomeDialog();
+
+private:
+    Ui::WelcomeDialog *ui;
+//    LandWidget landwidget;      //登录界面的实例
+    MainWidget mainwidget;
+
+private slots:
+    void goon();
+};
+
+#endif // WELCOMEDIALOG_H
